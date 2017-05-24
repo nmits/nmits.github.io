a58377374e8cc3b4417a59df3f43ba05cae86ed6
Noah Mitsuhashi <nmitsuha@usc.edu>
2017-05-23 21:46:41 -0700

    Scrolling intro

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -35,3 +35,10 @@ $( '#dataLink' ).on('click', function(event) {
          }, 1500);
   });
 
+
+/**
+0%, 12.66%, 16.66%, 100% {transform:translate3d(0,0,0);}
+  29.32%, 33.32%, 45.98% {transform:translate3d(0,-25%,0);}
+  49.98%,62.64%, 66.64% {transform:translate3d(0,-50%,0);}
+  79.3%, 83.3%, 95.96% {transform:translate3d(0,-75%,0);}
+  */
