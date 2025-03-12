8bad4072e3753e7c23a3c6b89ead8c29c6a55af9
Noah Mitsuhashi <me@noahmitsuhashi.io>
2025-03-11 22:34:29 -0700

    Updating to node.js

---
+++ old/js/smoothscroll.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href');
+      if (targetId !== '#') {
+        document.querySelector(targetId).scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+});
